b910631e1219a637e7eb5ac136e2b27204eebcb5
prithvipaddy <prithvipaddy@gmail.com>
2024-05-30 19:56:36 +0530

    upcoming schedule

--- lib/home_upcomingschedule/view.dart
+++ lib/home_upcomingschedule/view.dart
@@ -9,41 +9,46 @@ class UpcomingSchedule extends GetView<ScheduleController> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              RichText(
-                text: const TextSpan(
-                  text: "Upcoming Schedule",
-                  style: TextStyle(fontSize: 16, color: Colors.black),
-                  children: [
-                    TextSpan(
-                      text: "\n30-05-24",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ],
-                ),
+    return GetBuilder<ScheduleController>(
+      builder: (controller) => Stack(children: [
+        controller.addingEvent
+            ? AddScheduleItem()
+            : Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          text: "Upcoming Schedule",
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                          children: [
+                            TextSpan(
+                              text: "\n30-05-24",
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          controller.addingEvent = !controller.addingEvent;
+                          controller.update();
+                        },
+                        icon: const Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      height: 500,
+                      child: ListView(
+                        children: controller.items
+                            .map((e) => ScheduleItemCard(item: e))
+                            .toList(),
+                      )),
+                ],
               ),
-              IconButton(
-                onPressed: controller.addEvent,
-                icon: const Icon(Icons.add),
-              ),
-            ],
-          ),
-          Container(
-            height: 500,
-            child: GetBuilder<ScheduleController>(builder: (value) {
-              return ListView(
-                children:
-                    value.items.map((e) => ScheduleItemCard(item: e)).toList(),
-              );
-            }),
-          ),
-        ],
-      ),
+      ]),
     );
   }
 }
@@ -128,43 +133,91 @@ class ScheduleItemCard extends StatelessWidget {
   }
 }
 
-class AddScheduleItem extends StatelessWidget {
-  const AddScheduleItem({super.key});
+class AddScheduleItem extends GetView<ScheduleController> {
+  AddScheduleItem({super.key});
+  final TextEditingController title = TextEditingController();
+  final TextEditingController date = TextEditingController();
+  final TextEditingController time = TextEditingController();
+  final TextEditingController description = TextEditingController();
+  final TextEditingController participants = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          "Enter the details of the event to be added",
-          style: TextStyle(fontSize: 16),
-        ),
-        const TextField(
-            decoration: InputDecoration(hintText: "Enter the event title")),
-        const TextField(
-            decoration: InputDecoration(hintText: "Enter the date")),
-        const TextField(
-            decoration: InputDecoration(hintText: "Enter the time")),
-        const TextField(
-            decoration: InputDecoration(hintText: "Enter a description")),
-        const TextField(
-            decoration:
-                InputDecoration(hintText: "Enter the other participants")),
-        const SizedBox(
-          height: 10,
-        ),
-        Align(
-          alignment: Alignment.centerRight,
-          child: ElevatedButton(
-            onPressed: () {},
-            style: const ButtonStyle(
-                backgroundColor:
-                    MaterialStatePropertyAll(Color.fromARGB(255, 22, 39, 189))),
-            child: const Text("Add Event"),
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Enter the details of the event to be added",
+            style: TextStyle(fontSize: 16),
           ),
-        )
-      ],
+          TextFormField(
+            controller: title,
+            validator: (value) => GetUtils.isBlank(value ?? "") == true
+                ? "Please fill this field"
+                : null,
+            decoration:
+                const InputDecoration(hintText: "Enter the event title"),
+          ),
+          TextFormField(
+              controller: date,
+              validator: (value) => GetUtils.isBlank(value ?? "") == true
+                  ? "Please fill this field"
+                  : null,
+              decoration: const InputDecoration(hintText: "Enter the date")),
+          TextFormField(
+              controller: time,
+              validator: (value) => GetUtils.isBlank(value ?? "") == true
+                  ? "Please fill this field"
+                  : null,
+              decoration: const InputDecoration(hintText: "Enter the time")),
+          TextFormField(
+              controller: description,
+              validator: (value) => GetUtils.isBlank(value ?? "") == true
+                  ? "Please fill this field"
+                  : null,
+              decoration:
+                  const InputDecoration(hintText: "Enter a description")),
+          TextFormField(
+              controller: participants,
+              validator: (value) => GetUtils.isBlank(value ?? "") == true
+                  ? "Please fill this field"
+                  : null,
+              decoration: const InputDecoration(
+                  hintText: "Enter the other participants")),
+          const SizedBox(
+            height: 10,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  controller.items.add(ScheduleItem(
+                      title: title.text,
+                      date: date.text,
+                      description: description.text,
+                      name: participants.text,
+                      time: time.text));
+                  controller.addingEvent = !controller.addingEvent;
+                  controller.update();
+                  title.dispose();
+                  date.dispose();
+                  description.dispose();
+                  participants.dispose();
+                  time.dispose();
+                }
+              },
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 17, 67, 230))),
+              child: const Text("Add Event"),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
